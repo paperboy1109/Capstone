809a1d31f9d32346ab5b4789ccac74695782c252
Daniel Janiak <danieljjaniak@icloud.com>
2016-09-09 21:56:36 -0700

    Make the data table cells interactive based on the left/right panning gesture (implement movement and color changes)

--- Capstone/DataTableViewCell.swift
+++ Capstone/DataTableViewCell.swift
@@ -14,33 +14,55 @@ class DataTableViewCell: UITableViewCell {
     
     var initialCellCenter = CGPoint()
     
+    var deleteWhenPanGestureEnds = false
+    var changeSignOfDatumValue = false
+    
     // MARK: - Outlets
     
     @IBOutlet var datumTextField: UITextField!
     
-
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
         selectionStyle = .None
         
+        deleteWhenPanGestureEnds = false
+        
         /* Configure the cell labels */
         datumTextField.delegate = self
         datumTextField.contentVerticalAlignment = .Center
+        
         
         let recognizer = UIPanGestureRecognizer(target: self, action: #selector(DataTableViewCell.panGestureRecognized(_:)))
         recognizer.delegate = self
         addGestureRecognizer(recognizer)
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     // MARK: - Helpers
+    
+    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        
+        if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
+            
+            let displacementByPan = panGestureRecognizer.translationInView(superview!)
+            
+            /* Ignore gestures that are mostly vertical so that the table cells still scroll well */
+            if fabs(displacementByPan.x) > fabs(displacementByPan.y) {
+                return true
+            }
+            return false
+        }
+        return false
+    }
     
     func panGestureRecognized(recognizer: UIPanGestureRecognizer) {
         
@@ -52,15 +74,46 @@ class DataTableViewCell: UITableViewCell {
         }
         
         if recognizer.state == .Changed {
-            datumTextField.backgroundColor = UIColor.redColor()
+            
+            print("State has changed")
+            print(frame.size.width)
+            print(frame.origin.x)
+            
+            let currentDisplacement = recognizer.translationInView(self)
+            
+            /* Re-locate the cell in the view */
+            center = CGPointMake(initialCellCenter.x + currentDisplacement.x, initialCellCenter.y)
+            
+            /* If the pan gesture is far enough to the left or right, set the respective value to true */
+            deleteWhenPanGestureEnds = frame.origin.x < -frame.size.width / 3.0
+            changeSignOfDatumValue = frame.origin.x > frame.size.width / 3.0
+            
+            
         }
         
         if recognizer.state == .Ended {
-            datumTextField.backgroundColor = UIColor.clearColor()
+            
+            print("Here is deleteWhenPanGestureEnds: \(deleteWhenPanGestureEnds)")
+            
+            let initialCellFrame = CGRect(x: 0, y: frame.origin.y, width: bounds.size.width, height: bounds.size.height)
+            
+            if self.deleteWhenPanGestureEnds {
+                datumTextField.backgroundColor = UIColor.redColor()
+                UIView.animateWithDuration(0.4, animations: {self.frame = initialCellFrame})
+                // TODO: create a protocol and delegate to remove the cell from the table view
+                
+            } else if self.changeSignOfDatumValue {
+                datumTextField.backgroundColor = UIColor.greenColor()
+                UIView.animateWithDuration(0.4, animations: {self.frame = initialCellFrame})
+            } else {
+                UIView.animateWithDuration(0.2, animations: {self.frame = initialCellFrame})
+            }
+            
+            
         }
         
     }
-
+    
 }
 
 extension DataTableViewCell: UITextFieldDelegate {

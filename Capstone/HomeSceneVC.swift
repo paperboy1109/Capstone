1cbd5e3fc3d0035b951c32c3ef08b96f3caabd9f
Daniel Janiak <danieljjaniak@icloud.com>
2016-09-19 14:52:16 -0700

    Update the look of the welcome screen plot

--- Capstone/HomeSceneVC.swift
+++ Capstone/HomeSceneVC.swift
@@ -24,7 +24,9 @@ class HomeSceneVC: UIViewController {
         super.viewDidLoad()
 
         /* Create some sample data */
-        let xValues = [-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0]
+        
+        let xValues = SequenceGenerator.createSequenceWithStartingPoint(-2.5, end: 2.5, numberOfSteps: 50)
+        
         let xValuesAsStrings = xValues.map { String(format: "%.2f", $0) }
         var yValues: [ChartDataEntry] = []
         for i in 0..<xValues.count {
@@ -35,29 +37,33 @@ class HomeSceneVC: UIViewController {
         /* Display the sample data in a plot */
         let lineChart_yValues = LineChartDataSet(yVals: yValues, label: "Line 1")
         lineChart_yValues.mode = .CubicBezier
-        lineChart_yValues.fillColor = UIColor.redColor()
+        lineChart_yValues.fillColor = UIColor(red: 96.0/255.0, green: 237.0/255.0, blue: 179.0/255.0, alpha: 1.0)
         lineChart_yValues.drawFilledEnabled = true
+        lineChart_yValues.drawCirclesEnabled = false
         
+        /* Set the data to display in the plot */
         let lineChartData = LineChartData(xVals: xValuesAsStrings, dataSets: [lineChart_yValues])
-        
         plotView.data = lineChartData
         
+        /* Configure the look of the plot */
+        plotView.leftAxis.axisMinValue = 0.0
+        plotView.rightAxis.axisMaxValue = 0.0        
+        plotView.legend.enabled = false
+        plotView.descriptionText = ""
+        plotView.userInteractionEnabled = false
+        /* Remove labels from the data points and axes */
+        plotView.data?.setValueTextColor(UIColor.clearColor())
+        plotView.leftAxis.labelTextColor = UIColor.clearColor()
+        plotView.xAxis.labelTextColor = UIColor.clearColor()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
+}
+
+extension HomeSceneVC {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
-    */
-
 }

00fad500d736ed2c6ec611b2a699f193ec8a09c0
Daniel Janiak <danieljjaniak@icloud.com>
2016-09-19 14:19:42 -0700

    Apply theme tint color

--- Capstone/AppDelegate.swift
+++ Capstone/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let sharedApplication = UIApplication.sharedApplication()
+        sharedApplication.delegate?.window??.tintColor = UIColor(red: 96.0/255.0, green: 237.0/255.0, blue: 179.0/255.0, alpha: 1.0)
+        
         return true
     }
 

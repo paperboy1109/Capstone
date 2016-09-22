9a77200d9561305cf370c1b804a369c07f934fa7
Daniel Janiak <danieljjaniak@icloud.com>
2016-09-21 21:58:07 -0700

    Update calculator UI and logic (defensive coding, display results, keep running total, perform basic arithmetic)

---
+++ Capstone/CalculatorModel.swift
@@ -0,0 +1,142 @@
+//
+//  CalculatorModel.swift
+//  Capstone
+//
+//  Created by Daniel J Janiak on 9/21/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import Foundation
+
+enum MathOperations: Int {
+    
+    // The raw values are set to match the button tags
+    case sqrt = 12, exponentiation, division, multiplication, subtraction, addition, equate, none
+}
+
+enum EditingOperations: Int {
+    // The raw values are set to match the button tags
+    case clear = 10, delete
+}
+
+struct CalculatorMathVariablePair {
+    
+    var savedValue: Double?
+    var activeValue: Double?
+    var hasSavedValue = false
+    var operation: MathOperations?
+    
+    init() {
+        self.savedValue = nil
+        self.activeValue = nil
+    }
+    
+    mutating func setActiveValue(value: Double) {
+        self.activeValue = value
+    }
+    
+    mutating func saveValue(value: Double) {
+        self.savedValue = value
+        setDoesHaveSavedValue()
+    }
+    
+    mutating func setDoesHaveSavedValue() {
+        self.hasSavedValue = true
+    }
+    
+    mutating func setOperation(rawValue: Int) {
+        
+        switch rawValue {
+            
+        case MathOperations.sqrt.rawValue:
+            self.operation = MathOperations.sqrt
+            
+        case MathOperations.exponentiation.rawValue:
+            self.operation = MathOperations.exponentiation
+            
+        case MathOperations.division.rawValue:
+            self.operation = MathOperations.division
+            
+        case MathOperations.multiplication.rawValue:
+            self.operation = MathOperations.multiplication
+            
+        case MathOperations.subtraction.rawValue:
+            self.operation = MathOperations.subtraction
+            
+        case MathOperations.addition.rawValue:
+            self.operation = MathOperations.addition
+            
+        default:
+            self.operation = MathOperations.none
+            
+        }
+        
+    }
+    
+    mutating func resetValues() {
+        self.hasSavedValue = false
+        self.savedValue = nil
+        self.activeValue = nil
+    }
+    
+    mutating func combineValues() {
+        
+        guard self.activeValue != nil && self.savedValue != nil && self.operation != nil else { return }
+        
+        switch self.operation! {
+        case MathOperations.exponentiation:
+            self.savedValue = 0.0
+        case MathOperations.division:
+            self.savedValue = self.savedValue! / self.activeValue!
+        case MathOperations.multiplication:
+            self.savedValue = self.savedValue! * self.activeValue!
+        case MathOperations.subtraction:
+            self.savedValue = self.savedValue! - self.activeValue!
+        case MathOperations.addition:
+            self.savedValue = self.savedValue! + self.activeValue!
+        default:
+            self.savedValue = 0.0
+        }
+        
+        self.activeValue = nil        
+        
+    }
+    
+    func getSavedValueAsString() -> String {
+        guard self.savedValue != nil else { return "" }
+        
+        return String(self.savedValue!)
+    }
+    
+    
+    
+    
+    
+    
+    mutating func updateValues(val1: Double?, val2: Double?) {
+        self.savedValue = val1
+        self.activeValue = val2
+    }
+    
+    mutating func storeValue(value: Double) {
+        if !self.hasSavedValue {
+            self.savedValue = value
+        } else {
+            self.activeValue = value
+        }
+    }
+    
+
+    
+
+    
+    mutating func setOperation(currentOperation: MathOperations) {
+        self.operation = currentOperation
+    }
+}
+
+enum CalculatorTasks {
+    
+    case blank, creatingNewNumber, updatingNumber, updatingSecondNumber, fatalError
+    
+}
--- Capstone/CalculatorVC.swift
+++ Capstone/CalculatorVC.swift
@@ -7,43 +7,247 @@
 //
 
 import UIKit
-import NumberMorphView
 
 class CalculatorVC: UIViewController {
     
+    // MARK: - Properties
+    
+    var currentCalculatorTask: CalculatorTasks!
+    var calculatorData = CalculatorMathVariablePair()
+    
+    var enteredValue1 = ""
+    
     // MARK: - Outlets
     
-    @IBOutlet var resultView: NumberMorphView!
-
+    @IBOutlet var displayLabel: UILabel!
+    
+    @IBOutlet var clearButton: UIButton!
+    @IBOutlet var delButton: UIButton!
+    @IBOutlet var sqrtButton: UIButton!
+    @IBOutlet var expButton: UIButton!
+    @IBOutlet var divideButton: UIButton!
+    @IBOutlet var multiplyButton: UIButton!
+    @IBOutlet var subtractButton: UIButton!
+    @IBOutlet var addButton: UIButton!
+    @IBOutlet var equalsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        resultView.currentDigit = 0
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        clearDisplayText()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     // MARK: - Actions
     
     @IBAction func numberTapped(sender: CalculatorNumberButton) {
         
-        resultView.nextDigit = sender.tag
+        enableMathOperations()
+        
+        guard displayLabel.text != nil else {return}
+        
+        let currentDisplayText = displayLabel.text!
+        let currentDigit = sender.tag
+        let currentDigitString = String(currentDigit)
+        let concatenatedString = currentDisplayText + currentDigitString
+        
+        /* Simply update the active value */
+        if calculatorData.operation == nil {
+            
+            if currentDisplayText == "0" || currentDisplayText == "" {
+                displayLabel.text = currentDigitString
+            } else {
+                displayLabel.text = concatenatedString
+            }
+            
+            updateActiveValue()
+            
+            /* Take the mathematical operation into account */
+        } else {
+            
+            /* The display label is showing the saved value and should not be modified but rather replaced */
+            if calculatorData.activeValue == nil {
+                
+                displayLabel.text = currentDigitString
+                
+                /* The display label is showing the active value and should be updated */
+            } else {
+                displayLabel.text = concatenatedString
+            }
+            
+            updateActiveValue()
+        }
+        
     }
-
+    
+    @IBAction func operationButtonTapped(sender: UIButton) {
+        
+        if sender.tag == EditingOperations.clear.rawValue {
+            
+            clearDisplayText()
+            calculatorData.resetValues()
+            
+        } else if sender.tag == EditingOperations.delete.rawValue {
+            
+            del()
+            
+        } else if sender.tag == MathOperations.equate.rawValue {
+            
+            if let currentStoredValue = calculatorData.activeValue {
+                
+                if let currentSecondStoredValue = calculatorData.savedValue {
+                    
+                    let result = currentStoredValue + currentSecondStoredValue
+                    
+                    displayLabel.text = String(result)
+                    
+                    calculatorData.updateValues(result, val2: nil)
+                    
+                } else {
+                    displayLabel.text = String(currentStoredValue)
+                }
+                
+                
+            }
+            
+            print("After tapping =, calculator data is \(calculatorData)")
+            
+            
+        } else {
+            
+            /* Ignore the button tap if no values have been entered */
+            guard calculatorData.activeValue != nil else { return }
+            
+            /* Ignore the button tap if the screen is currently blank */
+            guard displayLabel.text != "" else { return }
+            
+            disableMathOperations()
+            
+            calculatorData.setOperation(sender.tag)
+            
+            updateActiveValue()
+            
+            if calculatorData.hasSavedValue {
+                
+                calculatorData.combineValues()
+                let newResult = calculatorData.getSavedValueAsString()
+                displayLabel.text = newResult
+                
+            } else {
+                
+                saveDisplayedValue()
+                self.displayLabel.text = ""
+            }
+            
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    func clearDisplayText() {
+        displayLabel.text = "0"
+        currentCalculatorTask = CalculatorTasks.blank
+    }
+    
+    func setError() {
+        displayLabel.text = "Error"
+        calculatorData.resetValues()
+        currentCalculatorTask = CalculatorTasks.fatalError
+    }
+    
+    func del() {
+        
+        guard let currentText = displayLabel.text else {
+            return
+        }
+        
+        if currentText.characters.count  > 1 {
+            
+            let updatedText = String(currentText.characters.dropLast())
+            
+            displayLabel.text = updatedText
+            
+            updateActiveValue()
+            
+        } else {
+            
+            clearDisplayText()
+            
+            updateActiveValue()
+        }
+        
+        
+    }
+    
+    func updateActiveValue() {
+        
+        guard let newStringValue = displayLabel.text else {
+            setError()
+            return
+        }
+        
+        guard newStringValue.characters.count >= 1 else {
+            setError()
+            return
+        }
+        
+        if let newDoubleValue = Double(newStringValue) {
+            
+            calculatorData.setActiveValue(newDoubleValue)
+            
+        } else {
+            setError()
+        }
+        
+    }
+    
+    func saveDisplayedValue() {
+        
+        guard let newStringValue = displayLabel.text else {
+            setError()
+            return
+        }
+        
+        guard newStringValue.characters.count >= 1 else {
+            setError()
+            return
+        }
+        
+        if let newDoubleValue = Double(newStringValue) {
+            
+            calculatorData.saveValue(newDoubleValue)
+            
+        } else {
+            setError()
+        }
+    }
+    
+    
+    func disableMathOperations() {
+        clearButton.enabled = false
+        delButton.enabled = false
+        sqrtButton.enabled = false
+        expButton.enabled = false
+        divideButton.enabled = false
+        multiplyButton.enabled = false
+        subtractButton.enabled = false
+        addButton.enabled = false
+        equalsButton.enabled = false
+    }
+    
+    func enableMathOperations() {
+        clearButton.enabled = true
+        delButton.enabled = true
+        sqrtButton.enabled = true
+        expButton.enabled = true
+        divideButton.enabled = true
+        multiplyButton.enabled = true
+        subtractButton.enabled = true
+        addButton.enabled = true
+        equalsButton.enabled = true
+    }
+    
 }
 
 
